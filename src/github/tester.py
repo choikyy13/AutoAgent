137a018714cc7771684816f22b78da31e7c51440
choikyy13 <choikyy13@gmail.com>
2025-11-23 15:21:16 +0100

    testing out tester

--- src/github/tester.py
+++ src/github/tester.py
@@ -1,13 +1,11 @@
-from github_finder import select_best_repository
+from src.pdf.pdf_extractor import get_github_links_from_pdf
+from src.pdf.pdf_extractor import extract_text_local
+from src.pdf.pdf_extractor import download_pdf
+import src.github.github_finder as github_finder
 
-test_links = [
-    "https://github.com/hyperopt/hyperopt",
-    "https://github.com/Teoroo-CMC/PiNN",
-    "https://github.com/optuna/optuna",
-    "https://github.com/hyperopt/hyperopt"
-]
-    
-test_paper = "This paper introduces hyperopt..."
-    
-result = select_best_repository(test_links, test_paper)
+test_links = get_github_links_from_pdf("https://pdfs.semanticscholar.org/d4f4/9717c9adb46137f49606ebbdf17e3598b5a5.pdf")
+path = download_pdf("https://pdfs.semanticscholar.org/d4f4/9717c9adb46137f49606ebbdf17e3598b5a5.pdf")
+test_paper = extract_text_local(path)
+
+result = github_finder.select_best_repository(test_links, test_paper)
 print(f"\nResult: {result}")
