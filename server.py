7c120e7bc32c996f09314077c0faccce1dd3e8b8
choikyy13 <choikyy13@gmail.com>
2025-11-25 11:19:57 +0100

    server.py modification

--- server.py
+++ server.py
@@ -29,4 +29,4 @@ def run_analysis():
         return jsonify({"status": "error", "message": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(host='0.0.0.0', debug=True, port=5000)
